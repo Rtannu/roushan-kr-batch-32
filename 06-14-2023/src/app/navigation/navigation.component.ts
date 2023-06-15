143e305ff08c207bf275f668f67be97b0538850b
Rtannu <info.raj.mnit@gmail.com>
2023-06-14 23:07:57 -0500

    Assignment Done

---
+++ 06-14-2023/src/app/navigation/navigation.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navigation',
+  templateUrl: './navigation.component.html',
+  styleUrls: ['./navigation.component.css']
+})
+export class NavigationComponent {
+  isLogoutEnable=true;
+  isLoginEnable=false;
+
+  constructor() { }
+  
+
+  ngOnInit() {
+  }
+
+  callLogout(){
+    this.isLogoutEnable=!this.isLogoutEnable;
+    this.isLoginEnable=!this.isLoginEnable;
+  }
+
+  callLogin(){
+    this.isLogoutEnable=!this.isLogoutEnable;
+    this.isLoginEnable=!this.isLoginEnable;
+  }
+
+}
